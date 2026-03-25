41cc86bfc117557ebc02605542687b85aede649f
sai sarayu <sarayu2926@gmail.com>
2026-03-25 15:12:14 +0530

    done

---
+++ src/readability_demo.py
@@ -0,0 +1,28 @@
+# BAD CODE (example of poor readability)
+x = 10
+y = 20
+z = x + y
+print(z)
+
+
+# GOOD CODE (improved readability)
+
+# Calculate total marks of two subjects
+math_marks = 10
+science_marks = 20
+
+total_marks = math_marks + science_marks
+
+print("Total Marks:", total_marks)
+
+# BAD COMMENT
+a = 5  # assigning 5 to a
+
+
+# GOOD COMMENT
+# Calculate final price after adding tax
+price = 100
+tax = 10
+final_price = price + tax
+
+print("Final Price:", final_price)
